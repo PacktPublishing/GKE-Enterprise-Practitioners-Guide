589a0e62c12be0ffd41cc4207c0d87591b8eed3b
Tim Berry <timhberry@gmail.com>
2024-02-28 09:30:25 +0000

    Add chapter 2 scripts

---
+++ chapter02/README.md
@@ -0,0 +1,3 @@
+# Chapter 2 scripts
+
+These scripts can be used to run the commands described in chapter 2 to create a GKE cluster on AWS.
---
+++ chapter02/aws-cluster.sh
@@ -0,0 +1,34 @@
+AWS_CLUSTER=aws-cluster
+
+# Create the cluster
+
+gcloud container aws clusters create $AWS_CLUSTER \
+  --cluster-version 1.26.2-gke.1001 \
+  --aws-region us-east-1 \
+  --location us-east4 \
+  --fleet-project $PROJECT_ID \
+  --vpc-id $VPC_ID \
+  --subnet-ids $PRIVATE_SUBNET_ID_1,$PRIVATE_SUBNET_ID_2,$PRIVATE_SUBNET_ID_3 \
+  --pod-address-cidr-blocks 10.2.0.0/16 \
+  --service-address-cidr-blocks 10.1.0.0/16 \
+  --role-arn $API_ROLE_ARN \
+  --iam-instance-profile $CONTROL_PLANE_PROFILE \
+  --database-encryption-kms-key-arn $KMS_KEY_ARN \
+  --config-encryption-kms-key-arn $KMS_KEY_ARN \
+  --tags google:gkemulticloud:cluster=$AWS_CLUSTER
+
+# Create a node pool
+
+gcloud container aws node-pools create pool-0 \
+  --location us-east4 \
+  --cluster $AWS_CLUSTER \
+  --node-version 1.26.2-gke.1001 \
+  --min-nodes 1 \
+  --max-nodes 5 \
+  --max-pods-per-node 110 \
+  --root-volume-size 50 \
+  --subnet-id $PRIVATE_SUBNET_ID_1 \
+  --iam-instance-profile $NODE_POOL_PROFILE \
+  --config-encryption-kms-key-arn $KMS_KEY_ARN \
+  --ssh-ec2-key-pair gke-key \
+  --tags google:gkemulticloud:cluster=$AWS_CLUSTER
